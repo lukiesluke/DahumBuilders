91432d7a709259757b11a2dab6ab8a77836747e2
Luke <Luke@DESKTOP-TNGJ3F6>
2020-09-25 10:53:20 +0800

    Add project files.

---
+++ DahumBuilders/FormUserProfile.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormUserProfile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtMiddleName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(98, 27)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(209, 20)
+        Me.txtFirstName.TabIndex = 0
+        '
+        'txtMiddleName
+        '
+        Me.txtMiddleName.Location = New System.Drawing.Point(98, 53)
+        Me.txtMiddleName.Name = "txtMiddleName"
+        Me.txtMiddleName.Size = New System.Drawing.Size(209, 20)
+        Me.txtMiddleName.TabIndex = 1
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(98, 79)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(209, 20)
+        Me.txtLastName.TabIndex = 2
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(98, 130)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(209, 20)
+        Me.txtAddress.TabIndex = 4
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(98, 156)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 5
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(260, 156)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Middle"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 86)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Surname"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(47, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Address"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(179, 156)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 6
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(98, 104)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(100, 20)
+        Me.txtGender.TabIndex = 3
+        Me.txtGender.Text = "Male"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(48, 110)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Gender"
+        '
+        'FormUserProfile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(356, 193)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtMiddleName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "FormUserProfile"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "User Profile"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtMiddleName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ DahumBuilders/FormUserProfile.vb
@@ -0,0 +1,94 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class FormUserProfile
+    Dim connection As New MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=dahum_builders")
+
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        Dim insert_command As New MySqlCommand("INSERT INTO `db_user_profile`
+            (`first_name`,`middle_name`,`last_name`,`gender`,`address`) 
+            VALUES (@first_name,@middle_name,@last_name,@gender,@address)", connection)
+
+        insert_command.Parameters.Add("@first_name", MySqlDbType.VarChar).Value = txtFirstName.Text
+        insert_command.Parameters.Add("@middle_name", MySqlDbType.VarChar).Value = txtMiddleName.Text
+        insert_command.Parameters.Add("@last_name", MySqlDbType.VarChar).Value = txtLastName.Text
+        insert_command.Parameters.Add("@gender", MySqlDbType.VarChar).Value = txtGender.Text
+        insert_command.Parameters.Add("@address", MySqlDbType.VarChar).Value = txtAddress.Text
+
+        If execCommand(insert_command) Then
+            txtFirstName.Text = ""
+            txtMiddleName.Text = ""
+            txtLastName.Text = ""
+            txtGender.Text = ""
+            txtAddress.Text = ""
+            MessageBox.Show("Successfully Saved")
+
+        Else
+
+            MessageBox.Show("Data NOT Inserted")
+
+        End If
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim sql_command As New MySqlCommand("SELECT * FROM `db_user_profile` WHERE `Id` = @id", connection)
+        sql_command.Parameters.Add("@id", MySqlDbType.Int64).Value = txtGender.Text
+        Dim adapter As New MySqlDataAdapter(sql_command)
+        Dim table As New DataTable()
+
+        Try
+            adapter.Fill(table)
+
+            If table.Rows.Count > 0 Then
+                txtFirstName.Text = table.Rows(0)("first_name")
+                txtMiddleName.Text = table.Rows(0)("middle_name")
+                txtLastName.Text = table.Rows(0)("last_name")
+                txtGender.Text = table.Rows(0)("gender")
+                txtAddress.Text = table.Rows(0)("address")
+            Else
+                txtFirstName.Text = ""
+                txtMiddleName.Text = ""
+                txtLastName.Text = ""
+                txtGender.Text = ""
+                txtAddress.Text = ""
+                MessageBox.Show("No Data Found")
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show("ERROR: " & ex.Message)
+        End Try
+    End Sub
+
+    Function execCommand(ByVal cmd As MySqlCommand) As Boolean
+
+        If connection.State = ConnectionState.Closed Then
+            connection.Open()
+        End If
+
+        Try
+            If cmd.ExecuteNonQuery() = 1 Then
+                Return True
+
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+
+            MessageBox.Show("ERROR")
+            Return False
+
+        End Try
+
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+
+    End Function
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        End
+    End Sub
+
+End Class
