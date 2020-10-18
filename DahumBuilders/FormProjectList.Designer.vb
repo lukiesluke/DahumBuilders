5126a1c1b19fba6d27348e0f3483929f13316722
Luke Garces <mrlukiesluke@gmail.com>
2020-10-18 19:01:25 +0800

    Update project list search functionality

--- DahumBuilders/FormProjectList.Designer.vb
+++ DahumBuilders/FormProjectList.Designer.vb
@@ -28,18 +28,29 @@ Partial Class FormProjectList
         Me.ColumnHeaderBlock = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeaderLot = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeaderSQM = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeaderAssignUser = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeaderPrice = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeaderAssignUser = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtBlock = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cbbProjectName = New System.Windows.Forms.ComboBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.chkAvailable = New System.Windows.Forms.CheckBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ListViewProject
         '
         Me.ListViewProject.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeaderID, Me.ColumnHeaderProjectName, Me.ColumnHeaderBlock, Me.ColumnHeaderLot, Me.ColumnHeaderSQM, Me.ColumnHeaderPrice, Me.ColumnHeaderAssignUser})
+        Me.ListViewProject.Dock = System.Windows.Forms.DockStyle.Fill
         Me.ListViewProject.FullRowSelect = True
         Me.ListViewProject.GridLines = True
-        Me.ListViewProject.Location = New System.Drawing.Point(29, 76)
+        Me.ListViewProject.Location = New System.Drawing.Point(3, 68)
         Me.ListViewProject.Name = "ListViewProject"
-        Me.ListViewProject.Size = New System.Drawing.Size(1032, 457)
+        Me.ListViewProject.Size = New System.Drawing.Size(1065, 418)
         Me.ListViewProject.TabIndex = 0
         Me.ListViewProject.UseCompatibleStateImageBehavior = False
         Me.ListViewProject.View = System.Windows.Forms.View.Details
@@ -52,12 +63,12 @@ Partial Class FormProjectList
         'ColumnHeaderProjectName
         '
         Me.ColumnHeaderProjectName.Text = "Project Name"
-        Me.ColumnHeaderProjectName.Width = 110
+        Me.ColumnHeaderProjectName.Width = 120
         '
         'ColumnHeaderBlock
         '
         Me.ColumnHeaderBlock.Text = "Block"
-        Me.ColumnHeaderBlock.Width = 50
+        Me.ColumnHeaderBlock.Width = 70
         '
         'ColumnHeaderLot
         '
@@ -66,30 +77,118 @@ Partial Class FormProjectList
         '
         'ColumnHeaderSQM
         '
-        Me.ColumnHeaderSQM.Text = "sqm"
+        Me.ColumnHeaderSQM.Text = "SQM"
+        Me.ColumnHeaderSQM.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         Me.ColumnHeaderSQM.Width = 50
+        '
+        'ColumnHeaderPrice
+        '
+        Me.ColumnHeaderPrice.Text = "Price"
+        Me.ColumnHeaderPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.ColumnHeaderPrice.Width = 80
         '
         'ColumnHeaderAssignUser
         '
         Me.ColumnHeaderAssignUser.Text = "Assigned"
         Me.ColumnHeaderAssignUser.Width = 150
         '
-        'ColumnHeaderPrice
+        'TableLayoutPanel1
         '
-        Me.ColumnHeaderPrice.Text = "Price"
-        Me.ColumnHeaderPrice.Width = 80
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ListViewProject, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.3995!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.60049!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1071, 489)
+        Me.TableLayoutPanel1.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.chkAvailable)
+        Me.Panel1.Controls.Add(Me.txtBlock)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.cbbProjectName)
+        Me.Panel1.Controls.Add(Me.btnSearch)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(3, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1065, 59)
+        Me.Panel1.TabIndex = 1
+        '
+        'txtBlock
+        '
+        Me.txtBlock.Location = New System.Drawing.Point(526, 18)
+        Me.txtBlock.MaxLength = 5
+        Me.txtBlock.Name = "txtBlock"
+        Me.txtBlock.Size = New System.Drawing.Size(112, 26)
+        Me.txtBlock.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(472, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Block"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Project Name"
+        '
+        'cbbProjectName
+        '
+        Me.cbbProjectName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbbProjectName.FormattingEnabled = True
+        Me.cbbProjectName.Location = New System.Drawing.Point(128, 16)
+        Me.cbbProjectName.Name = "cbbProjectName"
+        Me.cbbProjectName.Size = New System.Drawing.Size(338, 28)
+        Me.cbbProjectName.TabIndex = 2
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(897, 15)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(151, 38)
+        Me.btnSearch.TabIndex = 1
+        Me.btnSearch.Text = "&Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'chkAvailable
+        '
+        Me.chkAvailable.AutoSize = True
+        Me.chkAvailable.Location = New System.Drawing.Point(655, 18)
+        Me.chkAvailable.Name = "chkAvailable"
+        Me.chkAvailable.Size = New System.Drawing.Size(177, 24)
+        Me.chkAvailable.TabIndex = 6
+        Me.chkAvailable.Text = "Show Available Only"
+        Me.chkAvailable.UseVisualStyleBackColor = True
         '
         'FormProjectList
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1089, 562)
-        Me.Controls.Add(Me.ListViewProject)
+        Me.ClientSize = New System.Drawing.Size(1098, 564)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "FormProjectList"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Project List"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -102,4 +201,12 @@ Partial Class FormProjectList
     Friend WithEvents ColumnHeaderAssignUser As ColumnHeader
     Friend WithEvents ColumnHeaderSQM As ColumnHeader
     Friend WithEvents ColumnHeaderPrice As ColumnHeader
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents cbbProjectName As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBlock As TextBox
+    Friend WithEvents chkAvailable As CheckBox
 End Class
--- DahumBuilders/FormProjectList.vb
+++ DahumBuilders/FormProjectList.vb
@@ -2,21 +2,58 @@
 
 Public Class FormProjectList
     Private Sub FormProjectList_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        load_project_list()
+        load_project_list(String.Empty)
+        load_ProjectName_combobox()
     End Sub
 
-    Private Sub load_project_list()
-        Dim table As New DataTable()
-
-        sql = "SELECT id, proj_name, block , lot, sqm, price, 
-        IFNULL((SELECT CONCAT(last_name , ', ', first_name ) FROM `db_user_profile` WHERE `db_user_profile`.`id`= assigned_userid),'') AS assigned_name  
-        FROM `db_project_list` INNER JOIN `db_project_item` ON `db_project_list`.id=`db_project_item`.`proj_id`"
-
+    Private Sub load_ProjectName_combobox()
+        sql = "SELECT proj_name FROM `db_project_list`"
         Connection()
         Try
             sqlCommand = New MySqlCommand(sql, sqlConnection)
             sqlDataReader = sqlCommand.ExecuteReader()
+            cbbProjectName.Items.Clear()
+            Do While sqlDataReader.Read = True
+                cbbProjectName.Items.Add(sqlDataReader("proj_name"))
+            Loop
+            sqlDataReader.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
+        End Try
+    End Sub
 
+    Private Sub load_project_list(ByVal projectName As String)
+        Dim checkAvailable As String = ""
+        If chkAvailable.Checked = True Then
+            checkAvailable = " AND p.`assigned_userid` <= 0"
+        End If
+
+        Connection()
+        If cbbProjectName.SelectedIndex = -1 Then
+            sql = "SELECT  l.`id`, l.`proj_name`, block , lot, sqm, price, 
+            IFNULL((SELECT CONCAT(last_name , ', ', first_name ) FROM `db_user_profile` WHERE `db_user_profile`.`id`= assigned_userid),'') AS assigned_name  
+            FROM `db_project_item` p INNER JOIN `db_project_list` l ON p.`proj_id`=l.`id` 
+            WHERE block LIKE @Block " & checkAvailable
+
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlCommand.Parameters.Add("@Block", MySqlDbType.VarChar).Value = "%" & txtBlock.Text.Trim & "%"
+            sqlDataReader = sqlCommand.ExecuteReader()
+        Else
+            sql = "SELECT  l.`id`, l.`proj_name`, block , lot, sqm, price, 
+            IFNULL((SELECT CONCAT(last_name , ', ', first_name ) FROM `db_user_profile` WHERE `db_user_profile`.`id`= assigned_userid),'') AS assigned_name  
+            FROM `db_project_item` p INNER JOIN `db_project_list` l ON p.`proj_id`=l.`id`
+            WHERE `proj_name` LIKE @projName AND block LIKE @Block " & checkAvailable
+
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlCommand.Parameters.Add("@projName", MySqlDbType.VarChar).Value = "%" & cbbProjectName.SelectedItem & "%"
+            sqlCommand.Parameters.Add("@Block", MySqlDbType.VarChar).Value = "%" & txtBlock.Text.Trim & "%"
+            sqlDataReader = sqlCommand.ExecuteReader()
+        End If
+
+        Try
             Dim item As ListViewItem
             ListViewProject.Items.Clear()
             Do While sqlDataReader.Read = True
@@ -38,5 +75,24 @@ Public Class FormProjectList
             sqlCommand.Dispose()
             sqlConnection.Close()
         End Try
+    End Sub
+
+    Private Sub cbbProjectName_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbbProjectName.SelectedIndexChanged
+        txtBlock.Text = String.Empty
+        load_project_list(cbbProjectName.SelectedItem)
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        load_project_list(cbbProjectName.SelectedItem)
+    End Sub
+
+    Private Sub txtBlock_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtBlock.KeyPress
+        If Not Char.IsNumber(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub chkAvailable_CheckedChanged(sender As Object, e As EventArgs) Handles chkAvailable.CheckedChanged
+        btnSearch.PerformClick()
     End Sub
 End Class
