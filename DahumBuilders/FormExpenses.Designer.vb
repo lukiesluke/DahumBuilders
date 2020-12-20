f20397bbce7a356bcb7e41ca9aadf5fdfd8a6d98
Luke Garces <mrlukiesluke@gmail.com>
2020-12-20 17:10:16 +0800

    added expenses

--- DahumBuilders/FormExpenses.Designer.vb
+++ DahumBuilders/FormExpenses.Designer.vb
@@ -27,16 +27,36 @@ Partial Class FormExpenses
         Me.ColumnHeaderProjectName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeaderDate = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeaderTotalCash = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.dt = New System.Windows.Forms.DateTimePicker()
+        Me.cbbType = New System.Windows.Forms.ComboBox()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.txtCashoutAmount = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblTotalCashin = New System.Windows.Forms.Label()
+        Me.lblProjectID = New System.Windows.Forms.Label()
+        Me.ColumnHeaderTotalDeduction = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ListViewExpenses = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeaderDescription = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeaderAmount = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeaderType = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeaderExpDate = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.SuspendLayout()
         '
         'ListView1
         '
-        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeaderID, Me.ColumnHeaderProjectName, Me.ColumnHeaderDate, Me.ColumnHeaderTotalCash})
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeaderID, Me.ColumnHeaderProjectName, Me.ColumnHeaderDate, Me.ColumnHeaderTotalCash, Me.ColumnHeaderTotalDeduction})
         Me.ListView1.FullRowSelect = True
         Me.ListView1.GridLines = True
-        Me.ListView1.Location = New System.Drawing.Point(68, 40)
+        Me.ListView1.Location = New System.Drawing.Point(21, 84)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(718, 303)
+        Me.ListView1.Size = New System.Drawing.Size(688, 250)
         Me.ListView1.TabIndex = 0
         Me.ListView1.UseCompatibleStateImageBehavior = False
         Me.ListView1.View = System.Windows.Forms.View.Details
@@ -49,29 +69,204 @@ Partial Class FormExpenses
         'ColumnHeaderProjectName
         '
         Me.ColumnHeaderProjectName.Text = "Project Name"
-        Me.ColumnHeaderProjectName.Width = 200
+        Me.ColumnHeaderProjectName.Width = 170
         '
         'ColumnHeaderDate
         '
         Me.ColumnHeaderDate.Text = "Date"
-        Me.ColumnHeaderDate.Width = 100
+        Me.ColumnHeaderDate.Width = 80
         '
         'ColumnHeaderTotalCash
         '
         Me.ColumnHeaderTotalCash.Text = "Total Cash"
         Me.ColumnHeaderTotalCash.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
-        Me.ColumnHeaderTotalCash.Width = 150
+        Me.ColumnHeaderTotalCash.Width = 100
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(168, 37)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(90, 41)
+        Me.btnSearch.TabIndex = 7
+        Me.btnSearch.Text = "S&earch"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'dt
+        '
+        Me.dt.CustomFormat = "MM/dd/yyyy"
+        Me.dt.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dt.Location = New System.Drawing.Point(23, 41)
+        Me.dt.Name = "dt"
+        Me.dt.Size = New System.Drawing.Size(139, 26)
+        Me.dt.TabIndex = 6
+        '
+        'cbbType
+        '
+        Me.cbbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbbType.FormattingEnabled = True
+        Me.cbbType.Location = New System.Drawing.Point(847, 36)
+        Me.cbbType.Name = "cbbType"
+        Me.cbbType.Size = New System.Drawing.Size(282, 28)
+        Me.cbbType.TabIndex = 8
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Location = New System.Drawing.Point(847, 150)
+        Me.txtDescription.Multiline = True
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(282, 137)
+        Me.txtDescription.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(715, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 20)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Expenses Type"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(715, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 20)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Description"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(856, 293)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(137, 41)
+        Me.btnSave.TabIndex = 12
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(999, 293)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(130, 41)
+        Me.btnCancel.TabIndex = 13
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'txtCashoutAmount
+        '
+        Me.txtCashoutAmount.Location = New System.Drawing.Point(847, 112)
+        Me.txtCashoutAmount.Name = "txtCashoutAmount"
+        Me.txtCashoutAmount.Size = New System.Drawing.Size(282, 26)
+        Me.txtCashoutAmount.TabIndex = 14
+        Me.txtCashoutAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(715, 115)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 20)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Cashout Amount"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(715, 81)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 20)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Total Cashin"
+        '
+        'lblTotalCashin
+        '
+        Me.lblTotalCashin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCashin.Location = New System.Drawing.Point(847, 77)
+        Me.lblTotalCashin.Name = "lblTotalCashin"
+        Me.lblTotalCashin.Size = New System.Drawing.Size(278, 27)
+        Me.lblTotalCashin.TabIndex = 17
+        Me.lblTotalCashin.Text = "Total Cashin"
+        Me.lblTotalCashin.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblProjectID
+        '
+        Me.lblProjectID.AutoSize = True
+        Me.lblProjectID.Location = New System.Drawing.Point(719, 195)
+        Me.lblProjectID.Name = "lblProjectID"
+        Me.lblProjectID.Size = New System.Drawing.Size(90, 20)
+        Me.lblProjectID.TabIndex = 18
+        Me.lblProjectID.Text = "lblProjectID"
+        '
+        'ColumnHeaderTotalDeduction
+        '
+        Me.ColumnHeaderTotalDeduction.Text = "Total Deduction"
+        Me.ColumnHeaderTotalDeduction.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.ColumnHeaderTotalDeduction.Width = 100
+        '
+        'ListViewExpenses
+        '
+        Me.ListViewExpenses.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeaderDescription, Me.ColumnHeaderAmount, Me.ColumnHeaderType, Me.ColumnHeaderExpDate})
+        Me.ListViewExpenses.FullRowSelect = True
+        Me.ListViewExpenses.GridLines = True
+        Me.ListViewExpenses.Location = New System.Drawing.Point(21, 378)
+        Me.ListViewExpenses.Name = "ListViewExpenses"
+        Me.ListViewExpenses.Size = New System.Drawing.Size(1108, 273)
+        Me.ListViewExpenses.TabIndex = 19
+        Me.ListViewExpenses.UseCompatibleStateImageBehavior = False
+        Me.ListViewExpenses.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Width = 0
+        '
+        'ColumnHeaderDescription
+        '
+        Me.ColumnHeaderDescription.Text = "Description"
+        Me.ColumnHeaderDescription.Width = 350
+        '
+        'ColumnHeaderAmount
+        '
+        Me.ColumnHeaderAmount.Text = "Amount"
+        Me.ColumnHeaderAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.ColumnHeaderAmount.Width = 100
+        '
+        'ColumnHeaderType
+        '
+        Me.ColumnHeaderType.Text = "Type"
+        Me.ColumnHeaderType.Width = 100
+        '
+        'ColumnHeaderExpDate
+        '
+        Me.ColumnHeaderExpDate.Text = "Date"
+        Me.ColumnHeaderExpDate.Width = 80
         '
         'FormExpenses
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(865, 383)
+        Me.ClientSize = New System.Drawing.Size(1141, 663)
+        Me.Controls.Add(Me.ListViewExpenses)
+        Me.Controls.Add(Me.lblProjectID)
+        Me.Controls.Add(Me.lblTotalCashin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtCashoutAmount)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.cbbType)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.dt)
         Me.Controls.Add(Me.ListView1)
+        Me.MaximizeBox = False
         Me.Name = "FormExpenses"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FormExpenses"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -80,4 +275,24 @@ Partial Class FormExpenses
     Friend WithEvents ColumnHeaderProjectName As ColumnHeader
     Friend WithEvents ColumnHeaderDate As ColumnHeader
     Friend WithEvents ColumnHeaderTotalCash As ColumnHeader
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents dt As DateTimePicker
+    Friend WithEvents cbbType As ComboBox
+    Friend WithEvents txtDescription As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents txtCashoutAmount As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblTotalCashin As Label
+    Friend WithEvents lblProjectID As Label
+    Friend WithEvents ColumnHeaderTotalDeduction As ColumnHeader
+    Friend WithEvents ListViewExpenses As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeaderDescription As ColumnHeader
+    Friend WithEvents ColumnHeaderAmount As ColumnHeader
+    Friend WithEvents ColumnHeaderType As ColumnHeader
+    Friend WithEvents ColumnHeaderExpDate As ColumnHeader
 End Class
--- DahumBuilders/FormExpenses.vb
+++ DahumBuilders/FormExpenses.vb
@@ -1,27 +1,39 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class FormExpenses
+    Dim format As String = "yyyy-MM-dd"
     Private Sub FormExpenses_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        loadSales()
+        lblTotalCashin.Text = 0.ToString("N2")
+        txtCashoutAmount.Enabled = False
+        lblProjectID.Text = 0
+        btnSave.Enabled = False
+
+        loadCombo()
     End Sub
 
-    Private Sub loadSales()
+    Private Sub loadSales(dt As DateTimePicker)
         Connection()
         sql = "SELECT `proj_id`, (SELECT `proj_name` FROM `db_project_list` WHERE `id`= t.`proj_id`) proj_name,
-              `date_paid`, SUM(`paid_amount`) totalCash FROM `db_transaction` t WHERE `payment_type`=0 GROUP BY `proj_id`"
+              `date_paid`, SUM(`paid_amount`)-SUM(`commission`) AS totalCash, SUM(`commission`) AS totalDeduction FROM `db_transaction` t WHERE `payment_type`=0 AND `date_paid`=@dt GROUP BY `proj_id`"
 
         sqlCommand = New MySqlCommand(sql, sqlConnection)
-        'sqlCommand.Parameters.Add("@Block", MySqlDbType.VarChar).Value = txtBlock.Text.Trim
+        sqlCommand.Parameters.Add("@dt", MySqlDbType.VarChar).Value = dt.Value.ToString(format)
         sqlDataReader = sqlCommand.ExecuteReader()
         Try
             Dim item As ListViewItem
+            ListView1.Items.Clear()
             Do While sqlDataReader.Read = True
                 item = New ListViewItem(sqlDataReader("proj_id").ToString)
                 item.UseItemStyleForSubItems = False
                 item.SubItems.Add(sqlDataReader("proj_name"))
                 item.SubItems.Add(sqlDataReader("date_paid"))
                 Dim totalCash As Double = sqlDataReader("totalCash")
+                Dim totalDeduction As Double = sqlDataReader("totalDeduction")
                 With item.SubItems.Add(totalCash.ToString("N2"))
+                    .Font = New Font(ListView1.Font, FontStyle.Regular)
+                    .ForeColor = Color.Green
+                End With
+                With item.SubItems.Add(totalDeduction.ToString("N2"))
                     .Font = New Font(ListView1.Font, FontStyle.Regular)
                     .ForeColor = Color.Red
                 End With
@@ -30,5 +42,145 @@ Public Class FormExpenses
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        lblTotalCashin.Text = 0.ToString("N2")
+        txtCashoutAmount.Enabled = False
+        lblProjectID.Text = 0
+        loadSales(dt)
+    End Sub
+
+    Private Sub loadCombo()
+        sql = "SELECT `id`,`name` FROM `db_particular_type` WHERE id>5"
+        Connection()
+        Try
+            Cursor = Cursors.WaitCursor
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlDataReader = sqlCommand.ExecuteReader()
+
+            cbbType.DataSource = Nothing
+            cbbType.Items.Clear()
+
+            Dim comboSourceProjectName As New Dictionary(Of String, String)()
+            Do While sqlDataReader.Read = True
+                comboSourceProjectName.Add(sqlDataReader("id"), sqlDataReader("name"))
+            Loop
+
+            cbbType.DataSource = New BindingSource(comboSourceProjectName, Nothing)
+            cbbType.DisplayMember = "Value"
+            cbbType.ValueMember = "Key"
+
+            sqlDataReader.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
+            Cursor = Cursors.Default
+        End Try
+    End Sub
+    Private Sub loadDeduction(dt As DateTimePicker)
+        sql = "SELECT `id`, `date_paid`, `commission`, `description`, 
+        (SELECT `name` FROM `db_particular_type` WHERE `id`= `particular`) AS particular
+        FROM `db_transaction` WHERE `particular`>5 AND `date_paid`=@dt AND `proj_id`=@ProjID"
+
+        Connection()
+        sqlCommand = New MySqlCommand(sql, sqlConnection)
+        sqlCommand.Parameters.Add("@dt", MySqlDbType.VarChar).Value = dt.Value.ToString(format)
+        sqlCommand.Parameters.Add("@ProjID", MySqlDbType.VarChar).Value = lblProjectID.Text.Trim
+        sqlDataReader = sqlCommand.ExecuteReader()
+
+        Try
+            Cursor = Cursors.WaitCursor
+            Dim item As ListViewItem
+            ListViewExpenses.Items.Clear()
+            Do While sqlDataReader.Read = True
+                item = New ListViewItem(sqlDataReader("id").ToString)
+                item.UseItemStyleForSubItems = False
+                item.SubItems.Add(sqlDataReader("description"))
+                item.SubItems.Add(sqlDataReader("commission"))
+                item.SubItems.Add(sqlDataReader("particular"))
+                item.SubItems.Add(sqlDataReader("date_paid"))
+                ListViewExpenses.Items.Add(item)
+            Loop
+
+            sqlDataReader.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
+            Cursor = Cursors.Default
+        End Try
+    End Sub
+    Private Sub txtCashoutAmount_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtCashoutAmount.KeyPress
+        If (Not e.KeyChar = ChrW(Keys.Back) And ("0123456789.").IndexOf(e.KeyChar) = -1) Or (e.KeyChar = "." And txtCashoutAmount.Text.ToCharArray().Count(Function(c) c = ".") > 0) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub ListView1_Click(sender As Object, e As EventArgs) Handles ListView1.Click, ListView1.KeyUp
+        If ListView1.Items.Count > 0 Then
+            btnSave.Enabled = False
+            lblTotalCashin.Text = ListView1.SelectedItems(0).SubItems(3).Text
+            lblProjectID.Text = ListView1.SelectedItems(0).Text
+            loadDeduction(dt)
+            If Double.Parse(lblTotalCashin.Text) > 0 Then
+                txtCashoutAmount.Enabled = True
+            Else
+                txtCashoutAmount.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim particularID As String = DirectCast(cbbType.SelectedItem, KeyValuePair(Of String, String)).Key
+
+        sql = "INSERT INTO `db_transaction` (`date_paid`,`commission`,`particular`, `description`, `proj_id`) VALUES
+        (@DatePaid, @Commission, @Particular, @Description, @ProjID)"
+
+        Connection()
+        Try
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlCommand.Parameters.Add("@DatePaid", MySqlDbType.VarChar).Value = dt.Value.ToString(format)
+            sqlCommand.Parameters.Add("@Commission", MySqlDbType.Double).Value = Double.Parse(txtCashoutAmount.Text.Trim)
+            sqlCommand.Parameters.Add("@Particular", MySqlDbType.Int64).Value = particularID
+            sqlCommand.Parameters.Add("@Description", MySqlDbType.VarChar).Value = txtDescription.Text.Trim
+            sqlCommand.Parameters.Add("@ProjID", MySqlDbType.Int64).Value = lblProjectID.Text.Trim
+
+            If sqlCommand.ExecuteNonQuery() = 1 Then
+                sqlCommand.Dispose()
+                loadSales(dt)
+                loadDeduction(dt)
+                lblTotalCashin.Text = 0.ToString("N2")
+                txtCashoutAmount.Text = 0.ToString("N2")
+                txtDescription.Text = ""
+                btnSave.Enabled = False
+
+                MessageBox.Show("Expenses successfully save.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Expenses: " & ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
+        End Try
+    End Sub
+
+    Private Sub txtCashoutAmount_KeyUp(sender As Object, e As KeyEventArgs) Handles txtCashoutAmount.KeyUp
+        If txtCashoutAmount.Text.Length > 0 And txtCashoutAmount.Text <> "." Then
+            Dim cashin As Double = Double.Parse(lblTotalCashin.Text)
+            Dim cashout As Double = Double.Parse(txtCashoutAmount.Text)
+            If cashout > 0 And cashout <= cashin Then
+                btnSave.Enabled = True
+            Else
+                btnSave.Enabled = False
+            End If
+        End If
     End Sub
 End Class
