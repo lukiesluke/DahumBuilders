94324ad5ab5f511723dd2c5dd54936a7fa1177db
Luke Garces <mrlukiesluke@gmail.com>
2020-12-22 11:30:48 +0800

    update summary report

--- DahumBuilders/FormCRptSummaryReport.vb
+++ DahumBuilders/FormCRptSummaryReport.vb
@@ -14,7 +14,7 @@ Public Class FormCRptSummaryReport
         (SELECT IFNULL(SUM(it.`paid_amount`),0) FROM `db_transaction` it WHERE it.`proj_id`=t.`proj_id` AND  it.`payment_type`=1 AND it.`date_paid` BETWEEN @DateFrom AND @DateTo) AS 'check',
         (SELECT IFNULL(SUM(it.`paid_amount`),0) FROM `db_transaction` it WHERE it.`proj_id`=t.`proj_id` AND  it.`payment_type`=2 AND it.`date_paid` BETWEEN @DateFrom AND @DateTo) AS 'bankTransfer',
         (SELECT IFNULL(SUM(it.`commission`),0) FROM `db_transaction` it WHERE it.`proj_id`=t.`proj_id` AND it.`date_paid` BETWEEN @DateFrom AND @DateTo) AS 'commission',
-        (SELECT IFNULL(SUM(it.`paid_amount`)-SUM(it.`commission`),0) FROM `db_transaction` it WHERE it.`proj_id`=t.`proj_id` AND it.`date_paid` BETWEEN @DateFrom AND @DateTo) AS 'total'
+        (SELECT IFNULL(SUM(it.`paid_amount`)-SUM(it.`commission`),0) FROM `db_transaction` it WHERE it.`proj_id`=t.`proj_id` AND it.`payment_type`=0 AND it.`date_paid` BETWEEN @DateFrom AND @DateTo) AS 'total'
         FROM `db_project_list` l LEFT JOIN `db_transaction` t ON l.id = t.`proj_id` GROUP BY l.`id` ORDER BY l.`proj_name` ASC"
         Connection()
         Try
