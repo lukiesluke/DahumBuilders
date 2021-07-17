f4f86801814b4597d4054e4f5e7d1a82aa3493d1
Luke <Luke@DESKTOP-DMUER9F>
2021-07-17 22:42:55 +0800

    Update transaction history for single project name

--- DahumBuilders/FormCRptTransaction.Designer.vb
+++ DahumBuilders/FormCRptTransaction.Designer.vb
@@ -23,6 +23,13 @@ Partial Class FormCRptTransaction
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.cbbProjectName = New System.Windows.Forms.ComboBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'CrystalReportViewer1
@@ -32,23 +39,81 @@ Partial Class FormCRptTransaction
         Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
         Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.CrystalReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.CrystalReportViewer1.Margin = New System.Windows.Forms.Padding(2)
         Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
-        Me.CrystalReportViewer1.Size = New System.Drawing.Size(1173, 611)
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(782, 359)
         Me.CrystalReportViewer1.TabIndex = 0
         Me.CrystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
         '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.btnShow)
+        Me.Panel1.Controls.Add(Me.cbbProjectName)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(782, 38)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Project Name"
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(378, 8)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(75, 23)
+        Me.btnShow.TabIndex = 1
+        Me.btnShow.Text = "&Reload"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'cbbProjectName
+        '
+        Me.cbbProjectName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbbProjectName.FormattingEnabled = True
+        Me.cbbProjectName.Location = New System.Drawing.Point(90, 9)
+        Me.cbbProjectName.Name = "cbbProjectName"
+        Me.cbbProjectName.Size = New System.Drawing.Size(282, 21)
+        Me.cbbProjectName.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.CrystalReportViewer1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 38)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(782, 359)
+        Me.Panel2.TabIndex = 2
+        '
         'FormCRptTransaction
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1173, 611)
-        Me.Controls.Add(Me.CrystalReportViewer1)
+        Me.ClientSize = New System.Drawing.Size(782, 397)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "FormCRptTransaction"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Transaction History"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents cbbProjectName As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnShow As Button
 End Class
--- DahumBuilders/FormCRptTransaction.vb
+++ DahumBuilders/FormCRptTransaction.vb
@@ -1,16 +1,51 @@
 ﻿Imports MySql.Data.MySqlClient
 Imports CrystalDecisions.CrystalReports.Engine
-Imports CrystalDecisions.Shared
 Public Class FormCRptTransaction
     Public Property mUser As User = New User()
+    Private mouseDownClicked As Boolean
 
     Private Sub FormCRptTransaction_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Top = (20)
         Me.Left = (My.Computer.Screen.WorkingArea.Width \ 2) - (Me.Width \ 2)
         Me.Size = New Size(950, 650)
-        generate_report()
+        generate_listProjectCombo()
+        mouseDownClicked = False
     End Sub
+    Private Sub generate_listProjectCombo()
+        sql = "SELECT it.`item_id`, CONCAT( pl.`proj_name` ,' BLK', it.`block`,' L', it.`lot`) projectName FROM `db_transaction` t
+        INNER JOIN `db_project_list` pl ON pl.`id`= t.`proj_id`
+        INNER JOIN `db_project_item` it ON it.`item_id` = t.`proj_itemId`
+        WHERE t.`userid`=@userId AND t.`proj_itemId` GROUP BY item_id"
+        Connection()
+        Try
+            Cursor = Cursors.WaitCursor
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlCommand.Parameters.Add("@userId", MySqlDbType.Int64).Value = mUser._id
+            sqlDataReader = sqlCommand.ExecuteReader()
 
+
+            cbbProjectName.DataSource = Nothing
+            cbbProjectName.Items.Clear()
+
+            Dim comboSourceProjectName As New Dictionary(Of String, String)()
+            comboSourceProjectName.Add("0", "All Project")
+            Do While sqlDataReader.Read = True
+                comboSourceProjectName.Add(sqlDataReader("item_id"), sqlDataReader("projectName"))
+            Loop
+
+            cbbProjectName.DataSource = New BindingSource(comboSourceProjectName, Nothing)
+            cbbProjectName.DisplayMember = "Value"
+            cbbProjectName.ValueMember = "Key"
+
+            sqlDataReader.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
+            Cursor = Cursors.Default
+        End Try
+    End Sub
     Private Sub generate_report()
         Dim table As New DataTable()
         sql = "SELECT `official_receipt_no`,`paid_amount`, t.`discount_amount`, t.`penalty`, pt.`short_name` AS `payment_type`, 
@@ -77,7 +112,118 @@ Public Class FormCRptTransaction
             CrystalReportViewer1.Zoom(90)
         Catch ex As Exception
             MessageBox.Show("Generate Report: " & ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
         End Try
+
+    End Sub
+    Private Sub generate_report(itemID As String)
+
+        Dim table As New DataTable()
+        sql = "SELECT `official_receipt_no`,`paid_amount`, t.`discount_amount`, t.`penalty`, pt.`short_name` AS `payment_type`, 
+        IF(t.`part_no`=0,'',t.`part_no`) AS part_no, pa.`short_name` AS `particular`, `date_paid`, 
+        pl.`proj_name` , it.`block` , it.`lot` , it.`sqm` FROM `db_transaction` t 
+        INNER JOIN `db_payment_type` pt ON t.`payment_type` = pt.`id`
+        INNER JOIN `db_particular_type` pa ON t.`particular` = pa.`id` INNER JOIN `db_project_list` pl ON pl.`id`= t.`proj_id`
+        INNER JOIN `db_project_item` it ON it.`item_id` = t.`proj_itemId`
+        WHERE t.`userid`=@userId AND t.`proj_itemId`=@ItemID ORDER BY date_paid DESC, proj_name ASC, lot ASC"
+        Connection()
+        Try
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlCommand.Parameters.Add("@userId", MySqlDbType.Int64).Value = mUser._id
+            sqlCommand.Parameters.Add("@ItemID", MySqlDbType.Int32).Value = Convert.ToInt32(itemID)
+            sqlAdapter = New MySqlDataAdapter
+            With sqlAdapter
+                .SelectCommand = sqlCommand
+                .Fill(table)
+            End With
+
+            sql = "SELECT SUM(i.`price`) AS tcp, SUM(IFNULL((SELECT (`tcp`-SUM(`paid_amount`))-SUM(`discount_amount`) 
+            FROM `db_transaction` WHERE db_transaction.`proj_id`=i.`proj_id` AND db_transaction.`proj_itemId`=i.`item_id` AND i.`assigned_userid`=db_transaction.`userid`), i.`price` )) AS 'totalBalance'
+            FROM `db_project_list` l INNER JOIN `db_project_item` i ON l.`id`=i.`proj_id` WHERE i.`assigned_userid`=@userId  AND i.`item_id`=@ItemID"
+
+            sqlCommand = New MySqlCommand(sql, sqlConnection)
+            sqlCommand.Parameters.Add("@userId", MySqlDbType.Int64).Value = mUser._id
+            sqlCommand.Parameters.Add("@ItemID", MySqlDbType.Int32).Value = Convert.ToInt32(itemID)
+            sqlDataReader = sqlCommand.ExecuteReader()
+
+            Dim totalTcp As Double = 0
+            Dim totalBalance As Double = 0
+
+            Do While sqlDataReader.Read = True
+                If IsDBNull(sqlDataReader("totalBalance")) Then
+                Else
+                    totalTcp = sqlDataReader("tcp")
+                    totalBalance = sqlDataReader("totalBalance")
+                End If
+            Loop
+
+            Dim report As New crptTransaction
+            report.Load()
+
+            Dim txtHeaderCompanyName As TextObject = report.ReportDefinition.Sections("Section1").ReportObjects("txtHeaderCompanyName")
+            Dim txtHeaderCompanyAddress As TextObject = report.ReportDefinition.Sections("Section1").ReportObjects("txtCompanyAddress")
+
+            Dim name As TextObject = report.ReportDefinition.Sections("Section2").ReportObjects("txtName")
+            Dim mobile As TextObject = report.ReportDefinition.Sections("Section2").ReportObjects("txtMobile")
+            Dim address As TextObject = report.ReportDefinition.Sections("Section2").ReportObjects("txtAddress")
+            Dim txtTotalTCP As TextObject = report.ReportDefinition.Sections("Section5").ReportObjects("txtTotalTCP")
+            Dim txtTotalBalance As TextObject = report.ReportDefinition.Sections("Section5").ReportObjects("txtTotalBalance")
+
+            txtHeaderCompanyName.Text = ModuleConnection.CompanyName
+            txtHeaderCompanyAddress.Text = ModuleConnection.CompanyAddress
+
+            name.Text = mUser._name & " " & mUser._middleName & " " & mUser._surname
+            mobile.Text = mUser._mobile
+            address.Text = mUser._address
+            txtTotalTCP.Text = totalTcp.ToString("N2")
+            txtTotalBalance.Text = totalBalance.ToString("N2")
+
+            report.SetDataSource(table)
+
+            CrystalReportViewer1.ReportSource = report
+            CrystalReportViewer1.Refresh()
+            CrystalReportViewer1.Zoom(90)
+        Catch ex As Exception
+            MessageBox.Show("Generate Report: " & ex.Message)
+        Finally
+            sqlCommand.Dispose()
+            sqlConnection.Close()
+        End Try
+
     End Sub
 
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim key As String = DirectCast(cbbProjectName.SelectedItem, KeyValuePair(Of String, String)).Key
+        Dim value As String = DirectCast(cbbProjectName.SelectedItem, KeyValuePair(Of String, String)).Value
+
+        If key.Equals("0") Then
+            generate_report()
+        Else
+            generate_report(key)
+        End If
+
+    End Sub
+
+    Private Sub FormCRptTransaction_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        btnShow.PerformClick()
+    End Sub
+
+    Private Sub cbbProjectName_MouseUp(sender As Object, e As MouseEventArgs) Handles cbbProjectName.MouseUp
+        mouseDownClicked = True
+    End Sub
+
+    Private Sub cbbProjectName_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbbProjectName.SelectedIndexChanged
+        If mouseDownClicked = True Then
+            Dim key As String = DirectCast(cbbProjectName.SelectedItem, KeyValuePair(Of String, String)).Key
+            Dim value As String = DirectCast(cbbProjectName.SelectedItem, KeyValuePair(Of String, String)).Value
+
+            If key.Equals("0") Then
+                generate_report()
+            Else
+                generate_report(key)
+            End If
+        End If
+    End Sub
 End Class
